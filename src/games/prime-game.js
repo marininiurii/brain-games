a828a5555c0c30ec1c8b79e7d7cddef54e96b416
iurii marinin <marininpost@gmail.com>
2023-05-19 15:14:24 +0400

    fixed brain-prime

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,16 +1,22 @@
 import index from '../index.js';
 
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function runPrimeGame() {
   const getRandom = () => Math.floor(Math.random() * 100) + 1;
   const expression = getRandom();
   let expressionCheck = '';
-  for (let i = 2; i <= expression / 2; i += 1) {
-    if (expression % i === 0) {
-      expressionCheck = 'no';
-    } else {
-      expressionCheck = 'yes';
-    }
-  }
+  expressionCheck = isPrime(expression) ? 'yes' : 'no';
   return [expression, expressionCheck];
 }
 
